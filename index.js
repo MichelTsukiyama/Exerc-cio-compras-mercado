01f8b8168a290d4dabe9b1c6589a53b00a1a29be
Michel Tsukiyama <>
2021-05-23 20:00:52 +0000

    adicionado readme e comentários

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var preco_arroz_t1, preco_arroz_t2, preco_feijao_branco, preco_feijao_preto, preco_linguica, preco_lentilha, resposta, total_compras;
 
+//Entrada de preços
 preco_arroz_t1 = prompt('Insira o preço do kilo do arroz Tipo 1:');
 preco_arroz_t2 = prompt('Insira o preço do kilo do arroz Tipo 2:');
 preco_feijao_branco = prompt('Insira o preço do kilo do feijão branco:');
@@ -7,6 +8,7 @@ preco_feijao_preto = prompt('Insira o preço do kilo do feijão preto:');
 preco_linguica = prompt('Insira o preço do kilo da linguiça defumada:');
 preco_lentilha = prompt('Insira o preço do kilo da lentilha:');
 
+//Entrada e condições para saber quais itens comprar
 resposta = prompt('Tem arroz tipo 1?\n Responda S para SIM ou N para NÃO');
 
 if(resposta == 'S'){
@@ -25,5 +27,5 @@ if(resposta == 'S'){
     total_compras = (preco_lentilha*2)+preco_linguica;
   }
 }
-
+//Saída com valor total da lista
 alert('O preço da sua compra foi R$'+total_compras);
---
+++ readme.md
@@ -0,0 +1,3 @@
+Código de lista de mercado
+
+A mãe solicita que o filho compre alguns itens, mas há algumas condições e devemos calcular o valor final da lista de itens.
